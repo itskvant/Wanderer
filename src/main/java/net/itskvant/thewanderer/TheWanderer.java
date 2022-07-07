a71b341de05e72ae7510ab0bb12acaf62e50a52f
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-07-07 19:29:52 -0400

    fixed structure gen

--- src/main/java/net/itskvant/thewanderer/TheWanderer.java
+++ src/main/java/net/itskvant/thewanderer/TheWanderer.java
@@ -1,34 +1,19 @@
 package net.itskvant.thewanderer;
 
 import com.mojang.logging.LogUtils;
-import com.sun.jna.platform.win32.WinNT;
 import net.itskvant.thewanderer.item.ModItems;
 import net.itskvant.thewanderer.world.structure.ModStructures;
-import net.itskvant.thewanderer.world.structure.SkyStructures;
-import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.inventory.Slot;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 import top.theillusivec4.curios.api.SlotTypeMessage;
-import top.theillusivec4.curios.api.SlotTypePreset;
-
-import java.awt.*;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TheWanderer.MOD_ID)
--- src/main/java/net/itskvant/thewanderer/world/structure/ModStructures.java
+++ src/main/java/net/itskvant/thewanderer/world/structure/ModStructures.java
@@ -23,7 +23,7 @@ public class ModStructures {
      * Registers the base structure itself and sets what its path is. In this case,
      * this base structure will have the resourcelocation of thewanderer:sky_structures.
      */
-    public static final RegistryObject<StructureFeature<?>> SKY_STRUCTURES = DEFERRED_REGISTRY_STRUCTURE.register("sky_structures", SkyStructures::new);
+    public static final RegistryObject<StructureFeature<?>> NETHER_TENT = DEFERRED_REGISTRY_STRUCTURE.register("nether_tent", NetherTent::new);
 
 
     public static void register(IEventBus eventBus) {
